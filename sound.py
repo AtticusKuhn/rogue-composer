477cad2c54718fa0ebacb37c69df2c36d534f353
Atticus Kuhn <atticusmkuhn@atticsmkuhn.com>
2025-02-09 12:14:00 +0000

    add piano

--- sound.py
+++ sound.py
@@ -1,8 +1,12 @@
 import pygame
+import pygame.midi
+
 import numpy as np
 from constants import *
 from enum import Enum
 import time
+
+
 
 
 
@@ -17,7 +21,14 @@ class Note(Enum):
     F = "f"
     G = "g"
 
-
+# define all the constant values -----------------------------------------------
+device = 0     # device number in win10 laptop
+instrument = 9 # http://www.ccarh.org/courses/253/handout/gminstruments/
+note_Do = 48   # http://www.electronics.dit.ie/staff/tscarff/Music_technology/midi/midi_note_numbers_for_octaves.htm
+note_Re = 50
+note_Me = 52
+volume = 127
+wait_time = 0.5
 def toNote(note: str) -> Note:
     if note == "a":
         return Note.A
@@ -38,6 +49,10 @@ def toNote(note: str) -> Note:
 class SoundManager:
     def __init__(self):
         pygame.midi.init()
+        self.player = pygame.midi.Output(0)
+
+            # set the instrument -----------------------------------------------------------
+        self.player.set_instrument(instrument)
         pygame.mixer.init(frequency=SAMPLE_RATE, size=-16, channels=2, buffer=512)
         self.note_sounds = {
             Note.C: 0,  # A4
@@ -47,13 +62,31 @@ class SoundManager:
             Note.A: 8,  # E5
         }
 
+        self.midi_note_mapping = {
+            Note.C: 48,  # C3
+            Note.D: 50,  # D3
+            Note.E: 52,  # E3
+            Note.F: 53,  # F3
+            Note.G: 55,  # G3
+            Note.A: 57,  # A3
+            Note.B: 59,  # B3
+        }
+
+
     def play_note(self, note: Note):
-        if note in self.note_sounds:
-            player = pygame.midi.Output(self.note_sounds[note])
-            player.set_instrument(0)
-            player.note_on(64, 127)
-            time.sleep(0.1)
-            player.note_off(64, 127)
-            del player
+        if note in self.midi_note_mapping:
+            midi_note = self.midi_note_mapping[note]
+            filename = f"Piano{midi_note + 111 - 48}.ogg" # filenames start from 111, and C3 is 48
+            try:
+                sound = pygame.mixer.Sound(filename)
+                sound.play()
+            except pygame.error as e:
+                print(f"Error playing {filename}: {e}")
+
+    def quit(self):
+        del self.player
+        pygame.midi.quit()
+
+            # del device
             # octave = self.note_sounds[note] * 2
             # octave.play()
